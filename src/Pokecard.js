d43d157454218391e929cc31b5764e7e1f1d4a8a
Simon <bardrabbit@gmail.com>
2020-08-11 19:20:27 -0400

    present cards and the decks

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
-const Pokecard = () => {
+const formatId = (id) => {
+    if(id > 0 && id < 10) {
+        return (`00${id}`);
+    } else if(id >= 10 && id < 100) {
+        return (`0${id}`);
+    } else {
+        return id;
+    }
+}
+
+const Pokecard = ({id, name, type, base_experience}) => {
     return (
         <div className="Pokecard">
-            PokeCard
+            <h2>{name}</h2>
+            <img src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${formatId(id)}.png`} alt="pokemon-img"/>
+            <p>Type: {type} <br />
+               EXP: {base_experience}
+            </p>
+     
         </div>
     )
 }
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import Pokecard from './Pokecard';
+import './Pokedex.css';
 
 const Pokedex = () => {
     return (
         <div className="Pokedex">
-            <div>
+            <h1>
                 Pokedex
-            </div>
+            </h1>
             <div className="grid-container">
-
+                <div className="grid-item">
+                    <Pokecard id={4} name="Charmander" type="fire" base_experience={62}/>
+                </div>
+                <div className="grid-item">
+                    <Pokecard id={7} name="Squirtle" type="water" base_experience={63}/>
+                </div>
+                <div className="grid-item">
+                    <Pokecard id={11} name="Metapod" type="bug" base_experience={72}/>
+                </div>
+                <div className="grid-item">
+                    <Pokecard id={12} name="Butterfree" type="flying" base_experience={178}/>
+                </div>
+                <div className="grid-item">
+                    <Pokecard id={25} name="Pikachu" type="electric" base_experience={112}/>
+                </div>
+                <div className="grid-item">
+                    <Pokecard id={39} name="Jigglypuff" type="normal" base_experience={95}/>
+                </div>
+                <div className="grid-item">
+                    <Pokecard id={94} name="Gengar" type="poison" base_experience={225}/>
+                </div>
+                <div className="grid-item">
+                    <Pokecard id={133} name="Eevee" type="normal" base_experience={65}/>
+                </div>
+                
             </div>
         </div>
         
