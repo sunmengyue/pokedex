1eead2d845600d9984cac34c11cd2c3c63a760b5
Simon <bardrabbit@gmail.com>
2020-08-11 14:39:06 -0400

    set up the project skeleton

---
+++ src/Pokecard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Pokecard = () => {
+    return (
+        <div className="Pokecard">
+            PokeCard
+        </div>
+    )
+}
+
+export default Pokecard;
---
+++ src/Pokedex.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Pokecard from './Pokecard';
+
+const Pokedex = () => {
+    return (
+        <div className="Pokedex">
+            <div>
+                Pokedex
+            </div>
+            <div className="grid-container">
+
+            </div>
+        </div>
+        
+        );
+}
+
+export default Pokedex;
